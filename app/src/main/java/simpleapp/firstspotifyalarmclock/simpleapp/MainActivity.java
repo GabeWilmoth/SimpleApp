e12ed431fd63f9b2fcfc5aa2dddde147994df22c
GabeWilmoth <gawilmot@uncg.edu>
2018-07-24 16:08:33 -0400

    Added Timer, Button, and Now Plays A Spotify Playlist When The Alarm Goes Off.

--- app/src/main/java/simpleapp/firstspotifyalarmclock/simpleapp/MainActivity.java
+++ app/src/main/java/simpleapp/firstspotifyalarmclock/simpleapp/MainActivity.java
@@ -15,17 +15,33 @@ import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import java.util.Calendar;
+
 
 public class MainActivity extends Activity implements
         SpotifyPlayer.NotificationCallback, ConnectionStateCallback
 {
+
+    TimePicker alarmTimePicker;
+    PendingIntent pendingIntent;
+    AlarmManager alarmManager;
+
+
     // TODO: Replace with your client ID
     private static final String CLIENT_ID = "d017eced712141db99c4cea7d2b33211";
 
     // TODO: Replace with your redirect URI
     private static final String REDIRECT_URI = "yourcustomprotocol://callback";
 
-    private Player mPlayer;
+    private static Player mPlayer;
     // Request code that will be used to verify if the result comes from correct activity
     // Can be any integer
     private static final int REQUEST_CODE = 1337;
@@ -35,12 +51,44 @@ public class MainActivity extends Activity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        alarmTimePicker = (TimePicker) findViewById(R.id.timePicker);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
         // The only thing that's different is we added the 5 lines below.
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
         builder.setScopes(new String[]{"user-read-private", "streaming"});
         AuthenticationRequest request = builder.build();
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    public void OnToggleClicked(View view)
+    {
+        long time;
+        if (((ToggleButton) view).isChecked())
+        {
+            Toast.makeText(MainActivity.this, "ALARM ON", Toast.LENGTH_SHORT).show();
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            Intent intent = new Intent(this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+            time=(calendar.getTimeInMillis()-(calendar.getTimeInMillis()%60000));
+            if(System.currentTimeMillis()>time)
+            {
+                if (calendar.AM_PM == 0)
+                    time = time + (1000*60*60*12);
+                else
+                    time = time + (1000*60*60*24);
+            }
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 10000, pendingIntent);
+        }
+        else
+        {
+            alarmManager.cancel(pendingIntent);
+            Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -101,7 +149,7 @@ public class MainActivity extends Activity implements
         Log.d("MainActivity", "User logged in");
 
         // This is the line that plays a song.
-        mPlayer.playUri(null, "spotify:user:12172876507:playlist:4pJrgy05BSy28mEeylQdI0", 0, 0);
+        //mPlayer.playUri(null, "spotify:user:12172876507:playlist:4pJrgy05BSy28mEeylQdI0", 0, 0);
        //mPlayer.playUri(null, "spotify:track:0wFmawqundY8rOn9sZUqA6", 0, 0);
     }
 
@@ -123,5 +171,9 @@ public class MainActivity extends Activity implements
     @Override
     public void onConnectionMessage(String message) {
         Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    public static void testingPlay(){
+        mPlayer.playUri(null, "spotify:user:12172876507:playlist:4pJrgy05BSy28mEeylQdI0", 0, 0);
     }
 }
